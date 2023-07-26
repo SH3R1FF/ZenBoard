cf8e7a7d2dfad948d248a54fc0bb1ebb39fcb106
himanshu <himanshunageshwar444@gmail.com>
2023-07-26 13:16:51 +0530

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import Image from "next/image"
+import logo from "@/assets/zenboard.png"
+import {MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid"
+import Avatar from "react-avatar"
+import { useBoardStore } from "@/store/BoardStore"
+
+const Header = () => {
+
+  const [board,searchString, setSearchString] = useBoardStore((state) => [
+    state.board,
+    state.searchString,
+    state.setSearchString,
+
+  ]);
+
+  
+  return (
+  <header>
+    <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 justify-between rounded-b-2xl">
+
+      <div className="absolute 
+      top-0
+      left-0
+      w-full
+      h-96
+      bg-gradient-to-br
+      from-pink-400
+      to-[#0055D1]
+      rounded-md
+      blur-3xl
+      opacity-50
+      -z-50
+      "/>
+
+      <Image
+        src={logo}
+        alt="logo"
+        width={300}
+        height={100}
+        className="w-48 md:w-56 pb-10 md:pb-0 object-contain"
+        priority={true}
+        />
+
+      <div className="flex items-center space-x-5 flex-1 justify-end w-full">
+
+        <form className="flex flex-1 bg-white rounded-md shadow-md space-x-5 p-2 items-center md:flex-initial">
+          <MagnifyingGlassIcon className="h-6 w-6 text-gray-400"/>
+          <input type="text" 
+          placeholder="Search" 
+          className="flex-1 outline-none p-2"
+          value={searchString}
+          onChange={(e) => setSearchString(e.target.value)}
+          />
+          <button type="submit" hidden>Search</button>
+        </form>
+
+        <Avatar name="Dr Disrespect" size="50" round color="#0055D1" />
+
+      </div>
+
+    </div>
+
+    <div className="flex items-center justify-center px-5 py-2 md:py-5">
+      <p className="flex items-center text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1] p-5">
+        <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1 " /> 
+        GPT is summarizinng your task for the day... 
+      </p>
+    </div>
+  </header>
+  )
+}
+
+export default Header
