88a26e8cf9ba3d64f0c698e33f71ddf068e24c9d
himanshu <himanshunageshwar444@gmail.com>
2023-07-26 16:53:37 +0530

    error fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -65,7 +65,7 @@ const Header = () => {
     <div className="flex items-center justify-center px-5 py-2 md:py-5">
       <p className="flex items-center text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1] p-5">
         <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1 " /> 
-        GPT is summarizinng your task for the day... 
+        Welcome to ZenBoard: Your Ultimate Task Management App! Streamline, Organize, and Boost Productivity Today.
       </p>
     </div>
   </header>
